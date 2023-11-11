2ce0dd5be63e7e3abd51b181156cd15089f7ebeb
Storm <storm666death@gamil.com>
2023-11-12 00:54:06 +0300

    commit1

--- src/Main.java
+++ src/Main.java
@@ -31,13 +31,11 @@ public class Main {
         }
     }
 
-        private static void compareNumbers(){
-            int a = 5;
-            int b = 10;
-            if(a>=b){
-                System.out.println("a>=b");
+    private static void compareNumbers(){
+        int a = 5;
+        int b = 10;
+        if(a>=b){
+            System.out.println("a>=b");
         }else System.out.println("a<b");
     }
 }
-
-
