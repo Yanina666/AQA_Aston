57b3d9c8e61f27a1279580a5b0149da46be61b09
Storm <storm666death@gamil.com>
2023-11-12 11:31:09 +0300

    commit2

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,13 +1,40 @@
 public class Lesson2 {
-    public static void main(String[] args){
-        System.out.println(checkSum(5,6));
+    public static void main(String[] args) {
+        System.out.println(checkSum(5, 6));
+        positiveOrNegative(0);
+        System.out.println(checkPositiveOrNegative(8));
+        printString("Yanina", 5);
     }
+
     public static boolean checkSum(int a, int b) {
         int sum = a + b;
         if (sum > 10 && sum <= 20) {
             return true;
         } else {
             return false;
+        }
+    }
+
+    public static void positiveOrNegative(int a) {
+        if (a >= 0) {
+            System.out.println("This is a positive number");
+        } else {
+            System.out.println("This is a negative number");
+        }
+    }
+
+    public static boolean checkPositiveOrNegative(int a) {
+        if (a >= 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static void printString(String name, int a) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hi"+"," + ""+ name + "!");
+
         }
     }
 }
