310c25dd1b2386c2f01f88853ecc77d31404cd54
Storm <storm666death@gamil.com>
2023-11-12 17:38:32 +0300

    commit3

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Lesson2 {
     public static void main(String[] args) {
         System.out.println(checkSum(5, 6));
@@ -5,6 +7,9 @@ public class Lesson2 {
         System.out.println(checkPositiveOrNegative(8));
         printString("Yanina");
         System.out.println(checkYear(2024));
+        updateTheNumbers();
+        insertTheMassive();
+        multiplyTheNumbers();
     }
 
     public static boolean checkSum(int a, int b) {
@@ -34,15 +39,43 @@ public class Lesson2 {
 
     public static void printString(String name) {
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Hi"+"," + ""+ name + "!");
+            System.out.println("Hi" + "," + "" + name + "!");
         }
     }
 
-    public static boolean checkYear(int a){
-        if(a % 4 == 0 && a % 100 !=0 || a % 400 ==0){
+    public static boolean checkYear(int a) {
+        if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0) {
             return true;
         } else {
             return false;
         }
-}
-}
+    }
+
+    public static void updateTheNumbers() {
+        int[] nums = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        {
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = (nums[i] == 1) ? 0 : 1;
+            }
+            System.out.println("New massive:" + Arrays.toString(nums));
+        }
+    }
+    public static void insertTheMassive(){
+        int[] arr = new int[100];
+        for(int i =0; i<100; i++){
+            arr[i] = i + 1;
+            System.out.println("arr["+ i +"] = " +arr[i]);
+        }
+    }
+
+    public static void multiplyTheNumbers(){
+        int[] nums = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        {
+            for(int i = 0; i < nums.length; i++) {
+                if(nums[i] < 6) nums[i] *=2; {
+                    System.out.println(nums[i] + " ");
+                    }
+                }
+            }
+        }
+    }
