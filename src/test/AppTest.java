37b043c4b7c00ae297fb5f0abb872d0a100c4de7
Yanina666 <storm666death@gamil.com>
2024-01-08 23:35:57 +0300

    mts2

--- src/test/AppTest.java
+++ src/test/AppTest.java
@@ -12,8 +12,11 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-public class AppTest {
+import static org.testng.Assert.assertEquals;
+
+public class AppTest extends Main{
     private WebDriver driver = BrowserFactory.getDriver();
     private WebDriverWait wait;
 
@@ -39,13 +42,13 @@ public class AppTest {
         actions.moveToElement(blockTitle).perform();
         wait.until(ExpectedConditions.visibilityOf(blockTitle));
         String textOfBlock = blockTitle.getText();
-        Assert.assertEquals("Онлайн пополнение\nбез комиссии", textOfBlock);
+        assertEquals("Онлайн пополнение\nбез комиссии", textOfBlock);
     }
     @Test
     public void checkTheLogos() {
         WebElement paymentLogos = driver.findElement(By.className("pay__partners"));
         List<WebElement> imagesList = paymentLogos.findElements(By.tagName("img"));
-        Assert.assertEquals(false, imagesList.isEmpty());
+        assertEquals(false, imagesList.isEmpty());
         for (WebElement image : imagesList) {
             Assert.assertTrue(image.isDisplayed());
         }
@@ -54,7 +57,7 @@ public class AppTest {
     public void checkTheURL() {
         cookieClose();
         driver.findElement(By.xpath("//a[contains(text(),'Подробнее о сервисе')]")).click();
-        Assert.assertEquals("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/",
+        assertEquals("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/",
                 driver.getCurrentUrl());
     }
     @Test
@@ -74,5 +77,82 @@ public class AppTest {
         new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOfElementLocated
                         (By.className("container-fluid")));
         Assert.assertTrue(driver.findElement(By.className("container-fluid")).isDisplayed());
+    }
+    @Test
+    public void checkTheFiledCommunicationServices() { //проверяем надписи в Услугах связи
+        cookieClose();
+        driver.findElement(By.id("pay-section")).click();
+        assertEquals("Номер телефона",
+                driver.findElement(placeholderNumber).getAttribute("placeholder"));
+        assertEquals("Сумма", driver.findElement(placeholderSum).getAttribute("placeholder"));
+        assertEquals("E-mail для отправки чека", driver.findElement(placeholderEmail)
+                .getAttribute("placeholder"));
+    }
+    @Test
+    public void checkTheFieldHomeInternet(){ //проверяем надписи в разделе Домашний интернет
+        cookieClose();
+        driver.findElement(By.xpath("//p[@class='select__option'][contains(.,'Домашний интернет')]")).isEnabled();
+        driver.findElement(By.xpath("//p[@class='select__option'][text()='Домашний интернет']")).isSelected();
+        assertEquals("Номер абонента",
+                driver.findElement(placeholderNumberInternet).getAttribute("placeholder"));
+        assertEquals("Сумма", driver.findElement(placeholderSum).getAttribute("placeholder"));
+        assertEquals("E-mail для отправки чека", driver.findElement(placeholderEmail)
+                .getAttribute("placeholder"));
+    }
+    @Test
+    public void checkTheFieldCredit(){ //проверяем надписи в разделе Рассрочка
+        cookieClose();
+        driver.findElement(By.xpath("//li[@class='select__item active']")).isEnabled();
+        driver.findElement(By.xpath("//p[@class='select__option'][text()='Задолженность']")).isSelected();
+        assertEquals("Номер счета на 44", driver.findElement(placeholderCredit)
+                .getAttribute("placeholder"));
+        assertEquals("Сумма", driver.findElement(placeholderSum).getAttribute("placeholder"));
+        assertEquals("E-mail для отправки чека", driver.findElement(placeholderEmail)
+                .getAttribute("placeholder"));
+    }
+    @Test
+    public void checkTheFieldDebt(){ //проверяем надписи в разделе Задолженность
+        cookieClose();
+        driver.findElement(By.xpath("//li[@class='select__item active']")).isEnabled();
+        driver.findElement(By.xpath("//p[@class='select__option'][text()='Задолженность']")).isSelected();
+        assertEquals("Номер счета на 2073", driver.findElement(placeholderDebt)
+                .getAttribute("placeholder"));
+        assertEquals("Сумма", driver.findElement(placeholderSum).getAttribute("placeholder"));
+        assertEquals("E-mail для отправки чека", driver.findElement(placeholderEmail)
+                .getAttribute("placeholder"));
+    }
+    @Test
+    public void checkTheSum(){ //проверяем корректность отображения суммы после клика на кнопку Продолжить
+        cookieClose();
+        WebElement phone = driver.findElement(By.id("connection-phone"));
+        phone.clear();
+        phone.sendKeys("297777777");
+        WebElement sumInput = driver.findElement(By.id("connection-sum"));
+        sumInput.sendKeys("100");
+        WebElement email = driver.findElement(By.id("connection-email"));
+        email.sendKeys("Yanina@gmail.com");
+        cookieClose();
+        WebElement form = driver.findElement(By.id("pay-connection"));
+        WebElement button = form.findElement(By.xpath(".//button[contains(@class, 'button button__default') " +
+                "and text()='Продолжить']"));
+        button.click();
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOfElementLocated
+                (By.xpath("//div/iframe")));
+        Assert.assertTrue(driver.findElement(By.xpath("//div/iframe")).isDisplayed());
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='bepaid-iframe']")));
+        assertEquals("100.00 BYN", driver.findElement(headerPaymentAmount).getAttribute("outerText"));
+        assertEquals("Оплата: Услуги связи Номер:375297777777", driver.findElement(headerPaymentInfo)
+                .getAttribute("outerText"));
+        assertEquals("Номер карты", driver.findElement(cardNumber).getAttribute("outerText"));
+        assertEquals("Google Pay", driver.findElement(buttonGooglePay).getAttribute("ariaLabel"));
+        assertEquals("Yandex Pay", driver.findElement(buttonYandexPay).getAttribute("ariaLabel"));
+        assertEquals("Срок действия", driver.findElement(durationOfTheCard).getAttribute("outerText"));
+        assertEquals("CVC", driver.findElement(cvc).getAttribute("outerText"));
+        assertEquals("Имя держателя (как на карте)", driver.findElement(thePersonsName)
+                .getAttribute("outerText"));
+        assertEquals("Оплатить 100.00 BYN", driver.findElement(buttonSubmit)
+                .getAttribute("outerText"));
+        driver.switchTo().defaultContent();
+        driver.quit();
     }
 }
